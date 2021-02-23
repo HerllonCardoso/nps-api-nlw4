6b8ecce5e79fa55aacda9969b1b67d176072a2a9
Herllon Cardoso <herlloncardoso@pop-os.localdomain>
2021-02-23 09:03:23 -0300

    feat: createUser method

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
-import 'reflect-metadata'
-import './database'
-import express from "express";
+import 'reflect-metadata';
+import './database';
+import express from 'express';
+import { router } from './routes';
 const app = express();
 
+app.use(express.json());
+app.use(router);
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World - NLW04" });
-});
-
-app.post("/", (request, response) => {
-  return response.json({ message: "Os dados foram salvos com sucesso" })
-})
-
-app.listen(3333, () => console.log("Server is running"));
+app.listen(3333, () => console.log('Server is running on 3333'));
