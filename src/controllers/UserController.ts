a033b97ee0cbb7be0830f8f9bbdfeefee45a1647
Herllon Cardoso <cardoso.herllon@gmail.com>
2021-02-25 14:27:22 -0300

    set jest environment

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
-import { UsersRepository } from "../repositories/UserRepository";
+import { UsersRepository } from '../repositories/UserRepository';
 
 class UserController {
   async create(request: Request, response: Response) {
@@ -11,7 +11,7 @@ class UserController {
     const userAlreadyExists = await usersRepository.findOne({
       email,
     });
-    
+
     if (userAlreadyExists) {
       return response.status(400).json({
         error: 'User already exists',
--- src/routes.ts
+++ src/routes.ts
@@ -7,13 +7,9 @@ const router = Router();
 const userController = new UserController();
 const surveyController = new SurveysController();
 
-
 router.post('/users', userController.create);
 
 router.post('/surveys', surveyController.create);
 router.get('/surveys', surveyController.show);
-
-
-
 
 export { router };
